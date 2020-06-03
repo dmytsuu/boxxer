37b5a8385ea4f3fd08fd5fe126fcb6f0c95d707e
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-06-03 21:02:13 +0300

    Add Arranger class

--- lib/boxxer.rb
+++ lib/boxxer.rb
@@ -1,6 +1,34 @@
-require "boxxer/version"
+require 'boxxer/container'
+require 'boxxer/content'
+require 'boxxer/version'
 
 module Boxxer
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Arranger
+    attr_reader :contents, :containers
+
+    def initialize(weights, containers)
+      assign_contents(weights)
+      @containers = containers
+    end
+
+    def contents_weight
+      @contents_weight ||= contents.sum(&:weight).to_f
+    end
+
+    def max_limit
+      containers.find { |container| container[:limit] }[:limit]
+    end
+
+    def containers_required
+      (contents_weight / max_limit).ceil
+    end
+
+    private
+
+    def assign_contents(weights)
+      @contents = weights.sort.reverse.map { |weight| Content.new(weight) }
+    end
+  end
 end
