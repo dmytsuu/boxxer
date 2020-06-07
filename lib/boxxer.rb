52d63c951a41105e1e0ea5433262dff258a67821
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-06-07 13:34:43 +0300

    Rewrite arranger and container logic

--- lib/boxxer.rb
+++ lib/boxxer.rb
@@ -1,34 +1,47 @@
 require 'boxxer/container'
-require 'boxxer/content'
 require 'boxxer/version'
 
 module Boxxer
   class Error < StandardError; end
 
   class Arranger
-    attr_reader :contents, :containers
+    attr_reader :containers
 
-    def initialize(weights, containers)
-      assign_contents(weights)
-      @containers = containers
+    def initialize(containers:, weights:)
+      @available_containers = containers.sort { |container| container[:limit] }.reverse
+      @largest_container = @available_containers.last
+      @weights = weights.sort.reverse
+      @containers = []
     end
 
-    def contents_weight
-      @contents_weight ||= contents.sum(&:weight).to_f
-    end
-
-    def max_limit
-      containers.find { |container| container[:limit] }[:limit]
-    end
-
-    def containers_required
-      (contents_weight / max_limit).ceil
+    def call
+      raise Error, "Largest container limit is #{@largest_container[:limit]}, but maximum weight is #{@weights.max}" unless valid_options?
+      until @weights.empty? do
+        container = Container.new(matching_container)
+        complete_container(container)
+        @containers << container
+      end
     end
 
     private
 
-    def assign_contents(weights)
-      @contents = weights.sort.reverse.map { |weight| Content.new(weight) }
+    def conmplete_container(container)
+      loop do
+        fittable_weight = @weights.find { |weight| container.fittable?(weight) }
+        fittable_weight.nil? ? break : container.add_weight(@weights.delete_at(@weights.index(fittable_weight)))
+      end
+    end
+
+    def valid_options?
+      @largest_container[:limit] >= @weights.max
+    end
+
+    def max_weight
+      @weights.sum
+    end
+
+    def matching_container
+      @available_containers.find { |container| container[:limit] >= max_weight } || @largest_container
     end
   end
 end
--- lib/boxxer/container.rb
+++ lib/boxxer/container.rb
@@ -1,23 +1,38 @@
 module Boxxer
-  class Conteiner
-    attr_reader :width
-    attr_reader :height
-    attr_reader :length
-    attr_reader :contents
+  class Container
+    attr_reader :dimensions
     attr_reader :tare_weight
-    attr_reader :contents_weight
-    attr_reader :total_weight
+    attr_reader :net_weight
+    attr_reader :gross_weight
+    attr_writer :weights
 
-    def initialize(limit)
+    def initialize(width:, height:, length:, tare_weight:, limit:)
+      @width = width
+      @height = height
+      @length = length
       @limit = limit
+      @tare_weight = tare_weight
+      @weights = []
     end
 
-    def contents_weight
-      contents.sum(&:weight)
+    def dimensions
+       { width: width, height: height, length: length }
     end
 
-    def total_weight
-      contents_weight + tare_weight
+    def net_weight
+      @weights.sum
+    end
+
+    def gross_weight
+      (net_weight + @tare_weight).round(2)
+    end
+
+    def fittable?(weight)
+      @limit >= net_weight + weight
+    end
+
+    def add_weight(weight)
+      @weights.push(weight)
     end
   end
 end
